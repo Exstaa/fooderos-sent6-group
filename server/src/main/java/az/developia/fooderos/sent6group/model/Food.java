1958ddfa0f8d5f2104276b518f4c96311bda7341
Exstaa <exstagithub@gmail.com>
2024-01-26 13:59:24 +0400

    models and repositories have been created

---
+++ server/src/main/java/az/developia/fooderos/sent6group/model/Food.java
@@ -0,0 +1,26 @@
+package az.developia.fooderos.sent6group.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "fooderos_foods")
+public class Food {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+   private String name;
+   private String ingredients;
+   private Double price;
+   
+}
