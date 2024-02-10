9c97950086da04fd8a6ad535a70686618e02989b
Exstaa <exstagithub@gmail.com>
2024-02-10 18:03:22 +0400

    responsive adjustings

--- server/src/main/resources/static/js/home.js
+++ server/src/main/resources/static/js/home.js
@@ -5,6 +5,7 @@ let foodCardsElement = document.getElementById("food-cards");
 async function getFoods() {
     foods = await fetch('http://localhost:8080/api/foods/').then(response => response.json())
 
+
     console.log(foods);
 }
 
@@ -19,8 +20,8 @@ async function insertFoodsData(reload = false) {
     for (let i = 0; i < foods.length; i++) {
         let food = foods[i];
         let card = `
-        <div class="col-3">
-        <div class="card" style="width: 18rem;">
+        <div class="col-9 col-xxl-3 col-xl-3 col-lg-4 col-md-6 col-sm-9 mt-3">
+        <div class="card">
             <img src="${food.coverImg}" class="card-img-top" style="height:250px; object-fit:cover; object-position: 50% 50%;" alt="..." >
             <div class="card-body">
               <h5 class="card-title">${food.name}</h5>
